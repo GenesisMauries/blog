5491f6b24c0aeb5346ad57dc986830389697a4c9
GenesisMauries <exodo_54@hotmail.com>
2020-02-27 06:10:35 -0600

    Funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-fetch('js/data.json',{})
-.catch(error => console.log(`Tienes este problemita: ${error.message}`))
-.then((response) => response.json())
-.then(datos => drawData(datos))
+const articleContainer = document.getElementById('textBlog')
+const fetchData = () => {
+  fetch('js/data.json', {})
+    .catch(error => console.log(`Tienes este problemita: ${error.message}`))
+    .then((response) => response.json())
+    .then(datos => drawData(datos))
+}
 
 const drawData = (datos)=>{
   for(data of datos.slice(-3)){
@@ -9,20 +12,23 @@ const drawData = (datos)=>{
     <img src="${data.image}" class="card-img-top" alt="${data.title}" style="height: 250px;">
     <div class="card-body">
       <h5 class="card-title">${data.title}</h5>
-        <button type="button" class="float-right btn btn-primary" id="${data.id}" onClick="more(id, data)">
+        <button type="button" class="float-right btn btn-primary article" id="${data.id}">
           Leer artículo
         </button>
       </div>
     </div>`
   }
+  const articles = document.getElementsByClassName('article')
+  Array.from(articles).forEach(element => element.addEventListener('click', () => {showArticle(element.id, datos)}))
 }
 
-const more =(id,data)=>{
-  document.getElementById("textBlog").innerHTML+=`<h2 class="text-center mt-1">${data.title}</h2>
-  <h5 class="text-left">${data.category}</h5>
-  <p class="text-justify mt-3">${data.text}</p>`
-
-console.log(id,data)
+const showArticle = (id, data) => {
+  articleContainer.innerHTML = ''
+  const article = data.find(element => element.id === id)
+  articleContainer.innerHTML = `<h2 class="text-center mt-1">${article.title}</h2>
+  <h5 class="text-left">${article.category}</h5>
+  <p class="text-justify mt-3">${article.text}</p>`
 }
 
+window.onload = fetchData()
 
